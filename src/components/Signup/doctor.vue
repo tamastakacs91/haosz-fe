a5905233a7883cacd8490719ceb990d5a22cd20b
Takács Tamás <tamas.takacs91@gmail.com>
2020-07-22 22:26:05 +0200

    validation rules

--- src/components/Signup/doctor.vue
+++ src/components/Signup/doctor.vue
@@ -12,65 +12,87 @@
             <v-text-field
               label="NÉV"
               ref="name"
-              v-model="doctor.data.name"
               filled
               class="mt-4"
               :rules="[validationRules.required]"
               validate-on-blur
+              @input="$emit('input', $event, 'data', 'name')"
             ></v-text-field>
             <v-text-field
               label="ORVOSI PECSÉTSZÁM"
               ref="sealNumber"
-              v-model="doctor.data.sealNumber"
               filled
               :rules="[validationRules.required]"
               validate-on-blur
+              @input="$emit('input', $event, 'data', 'sealNumber')"
             ></v-text-field>
             <v-text-field
               label="MUNKAHELY"
               ref="workPlace"
               filled
+              @input="$emit('input', $event, 'data', 'workPlace')"
             ></v-text-field>
             <div class="d-flex flex-wrap">
               <v-text-field
                 label="MOBIL"
                 ref="mobile"
-                v-model="doctor.data.mobile"
                 filled
                 class="mr-md-5"
                 hint="Formátum: +36 20/30/70 xxxxxxx"
                 :rules="[validationRules.required, validationRules.mobile]"
                 validate-on-blur
+                @input="$emit('input', $event, 'data', 'mobile')"
               ></v-text-field>
               <v-text-field
                 label="E-MAIL"
                 ref="email"
-                v-model="doctor.data.email"
                 filled
                 type="email"
-                :rules="[validationRules.required]"
+                :rules="[validationRules.required, validationRules.email]"
                 validate-on-blur
+                @input="$emit('input', $event, 'data', 'email')"
               ></v-text-field>
             </div>
             <div class="mt-4 mb-4">Számlázási Adatok:</div>
-            <v-text-field label="CÉG NEVE" filled></v-text-field>
-            <v-text-field label="CÍM" filled></v-text-field>
-            <v-text-field label="ADÓSZÁM" filled></v-text-field>
-            <v-text-field label="KAPCSOLATTARTÓ NEVE" filled></v-text-field>
+            <v-text-field
+              label="CÉG NEVE"
+              filled
+              @input="$emit('input', $event, 'billing', 'name')"
+            ></v-text-field>
+            <v-text-field
+              label="CÍM"
+              filled
+              @input="$emit('input', $event, 'billing', 'address')"
+            ></v-text-field>
+            <v-text-field
+              label="ADÓSZÁM"
+              filled
+              @input="$emit('input', $event, 'billing', 'taxNumber')"
+            ></v-text-field>
+            <v-text-field
+              label="KAPCSOLATTARTÓ NEVE"
+              filled
+              @input="$emit('input', $event, 'billing', 'contact')"
+            ></v-text-field>
             <div class="d-flex flex-wrap">
               <v-text-field
                 label="MOBIL"
                 filled
                 class="mr-md-5"
                 hint="Formátum: +36 20/30/70 xxxxxxx"
+                @input="$emit('input', $event, 'billing', 'mobile')"
               ></v-text-field>
-              <v-text-field label="E-MAIL" filled type="email"></v-text-field>
+              <v-text-field
+                label="E-MAIL"
+                filled
+                type="email"
+                @input="$emit('input', $event, 'billing', 'email')"
+              ></v-text-field>
             </div>
             <div class="mt-4 mb-4">Jelszó:</div>
             <v-text-field
               label="JELSZÓ"
               ref="password"
-              v-model="doctor.password.password"
               filled
               :hint="'Legalább 8 karakter, tartalmaznia kell kisbetűt, nagybetűt, számot és egy különleges karaktert.'"
               :rules="[validationRules.required, validationRules.password]"
@@ -78,26 +100,26 @@
               :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
               :type="show1 ? 'text' : 'password'"
               @click:append="show1 = !show1"
-              @input="$emit('validatePassword', $event)"
+              @input="$emit('input', $event, 'password', 'password')"
             ></v-text-field>
             <div class="mb-4">Jelszó ismét:</div>
             <v-text-field
               label="JELSZÓ ISMÉT"
               ref="passwordAgain"
-              v-model="doctor.password.passwordAgain"
               filled
               :rules="[validationRules.required, validationRules.passwordAgain]"
               validate-on-blur
               :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
               :type="show2 ? 'text' : 'password'"
               @click:append="show2 = !show2"
+              @input="$emit('input', $event, 'password', 'passwordAgain')"
             ></v-text-field>
           </v-col>
         </v-card>
       </v-col>
       <v-row>
         <v-col cols="12" md="8" offset="md-2" class="text-center">
-          <v-btn color="primary" width="50%" @click="submit"
+          <v-btn color="primary" width="50%" @click="validate"
             >Regisztráció</v-btn
           >
         </v-col>
@@ -106,71 +128,30 @@
     </v-container>
     <slot name="more-info"></slot>
   </div>
-  <!-- <div>
-    <v-container>
-      <v-row>
-        <v-col class="text-center text-h5 text-md-h4 mt-5">Regisztráció Orvosoknak</v-col>
-      </v-row>
-      <v-col cols="12" md="8" offset="md-2">
-        <v-card outlined ref="form">
-          <v-col cols="12" md="10" offset="md-1">
-            <slot name="fields"></slot>
-          </v-col>
-        </v-card>
-      </v-col>
-      <v-row>
-        <v-col cols="12" md="8" offset="md-2" class="text-center">
-          <v-btn color="primary" width="50%" @click="$emit('click')">Regisztráció</v-btn>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>-->
 </template>
 
 <script>
 export default {
-  props: ['validationRules'],
+  props: ['validationRules', 'doctor'],
   data() {
     return {
-      doctor: {
-        data: {
-          name: '',
-          sealNumber: '',
-          workPlace: '',
-          mobile: '',
-          email: '',
-        },
-        billing: {
-          name: '',
-          address: '',
-          taxNumber: '',
-          contact: '',
-          mobile: '',
-          email: '',
-        },
-        password: {
-          password: '',
-          passwordAgain: '',
-        },
-      },
       show1: false,
       show2: false,
       errors: false,
     };
   },
   methods: {
-    checkFieldValiditiy(data) {
+    checkFieldValidity(data) {
       Object.keys(data).forEach((field) => {
         if (this.$refs[field].validate() === false) this.errors = true;
       });
     },
-    submit() {
+    validate() {
       this.errors = false;
-      this.checkFieldValiditiy(this.doctor.data);
-      this.checkFieldValiditiy(this.doctor.password);
-
+      this.checkFieldValidity(this.doctor.data);
+      this.checkFieldValidity(this.doctor.password);
       if (this.errors === true) return console.log('Hibás kitöltés!');
-      console.log(this.doctor.data);
+      this.$emit('validated');
     },
   },
 };
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -4,7 +4,9 @@
     <doctor
       v-if="$route.params.type === 'doktor'"
       :validationRules="rules"
-      @validatePassword="setDoctorPasswordToValidate"
+      :doctor="doctor"
+      @input="setDoctorData"
+      @validated="submitDoctorData"
     >
       <template v-slot:terms>
         <terms></terms>
@@ -22,24 +24,52 @@ export default {
   data() {
     return {
       doctorPassword: '',
+      doctor: {
+        data: {
+          name: '',
+          sealNumber: '',
+          workPlace: '',
+          mobile: '',
+          email: '',
+        },
+        billing: {
+          name: '',
+          address: '',
+          taxNumber: '',
+          contact: '',
+          mobile: '',
+          email: '',
+        },
+        password: {
+          password: '',
+          passwordAgain: '',
+        },
+      },
       rules: {
         required: (value) => !!value || 'Kötelező adat',
         mobile: (value) => {
           const pattern = /^[+](36)(20|30|31|50|70)([0-9]{7})$/;
           return pattern.test(value) || 'Érvénytelen formátum';
         },
+        email: (value) => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || 'Érvénytelen email formátum';
+        },
         password: (value) => {
           const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
           return pattern.test(value) || 'Érvénytelen jelszó';
         },
         passwordAgain: (value) =>
-          value === this.doctorPassword || 'A jelszó nem egyezik',
+          value === this.doctor.password.password || 'A jelszó nem egyezik',
       },
     };
   },
   methods: {
-    setDoctorPasswordToValidate(value) {
-      this.doctorPassword = value;
+    setDoctorData(value, nest, field) {
+      this.doctor[nest][field] = value;
+    },
+    submitDoctorData() {
+      console.log(this.doctor);
     },
   },
 };
