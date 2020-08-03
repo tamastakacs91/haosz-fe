6780d6d81529a749ef421442fe7743b50dbfe2d7
Takács Tamás <tamas.takacs91@gmail.com>
2020-08-03 08:57:52 +0200

    disabled options

--- src/components/Registration/doctor-payables.vue
+++ src/components/Registration/doctor-payables.vue
@@ -35,6 +35,38 @@
             </v-col>
           </v-row>
         </div>
+        <div v-if="date > 20200915">
+          <v-row>
+            <v-col cols="10">
+              <div>Rezidens, szakdolgozó (09.15-ig): 3.000 Ft + Áfa/fő</div>
+            </v-col>
+            <v-col cols="2">
+              <div class="d-flex flex-row-reverse">
+                <v-radio value="1" @change="$emit('selected', 3000)" disabled></v-radio>
+              </div>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="10">
+              <div>Háziorvos - HaOSz tag (09.15-ig): 3.000 Ft + Áfa/fő</div>
+            </v-col>
+            <v-col cols="2">
+              <div class="d-flex flex-row-reverse">
+                <v-radio value="2" @change="$emit('selected', 3000)" disabled></v-radio>
+              </div>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="10">
+              <div>Egyéb érdeklődő (09.15-ig): 5.000 Ft + Áfa/fő</div>
+            </v-col>
+            <v-col cols="2">
+              <div class="d-flex flex-row-reverse">
+                <v-radio value="3" @change="$emit('selected', 5000)" disabled></v-radio>
+              </div>
+            </v-col>
+          </v-row>
+        </div>
         <div>
           <v-row>
             <v-col cols="10">
@@ -42,11 +74,7 @@
             </v-col>
             <v-col cols="2">
               <div class="d-flex flex-row-reverse" v-if="date < 20200915">
-                <v-radio
-                  value="4"
-                  @change="$emit('selected', 10000)"
-                  disabled
-                ></v-radio>
+                <v-radio value="4" @change="$emit('selected', 10000)" disabled></v-radio>
               </div>
               <div class="d-flex flex-row-reverse" v-if="date > 20200915">
                 <v-radio value="4" @change="$emit('selected', 10000)"></v-radio>
@@ -69,9 +97,7 @@
       <v-row v-if="cost != 0">
         <v-col class="font-weight-black">Összesen</v-col>
         <v-col>
-          <div class="d-flex flex-row-reverse font-weight-black">
-            {{ cost }} Ft + Áfa
-          </div>
+          <div class="d-flex flex-row-reverse font-weight-black">{{ cost }} Ft + Áfa</div>
         </v-col>
       </v-row>
     </v-col>
