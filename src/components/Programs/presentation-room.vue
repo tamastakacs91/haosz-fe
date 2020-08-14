3fc9bd78d0baeb4c543517adc1984a271b99318a
Takács Tamás <tamas.takacs91@gmail.com>
2020-08-14 16:54:57 +0200

    programs extended

--- src/components/Programs/presentation-room.vue
+++ src/components/Programs/presentation-room.vue
@@ -1,15 +1,20 @@
 <template>
   <v-container class="mt-n8">
     <v-row class="mt-n2 mb-3">
-      <v-col class="text-md-h6"
-        >Szakmai-, orvostechnikai-, egészségpolitikai előadások felvételről és
-        élő adásban, élő szakmapolitikai fórum</v-col
-      >
+      <v-col class="text-md-h6">
+        Szakmai-, orvostechnikai-, egészségpolitikai előadások felvételről és
+        élő adásban, élő szakmapolitikai fórum
+      </v-col>
     </v-row>
     <v-row class="hidden-sm-and-down mb-3">
       <v-col>
         <v-simple-table>
           <template v-slot:default>
+            <thead>
+              <tr>
+                <th v-for="item in heads" :key="item">{{ item }}</th>
+              </tr>
+            </thead>
             <tbody>
               <tr v-for="item in data" :key="item.title">
                 <td class="font-weight-bold text-subtitle-1">
@@ -25,6 +30,13 @@
                   {{ item.title }}
                 </td>
                 <td v-else class="text-subtitle-1">{{ item.title }}</td>
+                <td>
+                  <div class="mt-1">{{ item.doctor }}</div>
+                  <div class="mt-1">
+                    {{ item.hospital }}
+                    <div class="mt-1">{{ item.department }}</div>
+                  </div>
+                </td>
               </tr>
             </tbody>
           </template>
@@ -49,8 +61,11 @@
                   <div>{{ item.title }}</div>
                 </td>
                 <td v-else class="text-subtitle-2">
-                  <div class="font-weight-bold">{{ item.time }}</div>
-                  <div>{{ item.title }}</div>
+                  <div class="font-weight-bold mt-1">{{ item.time }}</div>
+                  <div class="mt-1">{{ item.title }}</div>
+                  <div class="mt-1">{{ item.doctor }}</div>
+                  <div class="mt-1">{{ item.hospital }}</div>
+                  <div class="mt-1">{{ item.department }}</div>
                 </td>
               </tr>
             </tbody>
@@ -64,5 +79,10 @@
 <script>
 export default {
   props: ['data'],
+  data() {
+    return {
+      heads: ['Időpont', 'Előadás címe', 'Előadó'],
+    };
+  },
 };
 </script>
