22c86c390cace7ca351525090e3b73d6a592a274
Takács Tamás <tamas.takacs91@gmail.com>
2020-09-14 19:25:41 +0200

    vuetify minimized

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,4 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: true
-    }
-  }
 }
