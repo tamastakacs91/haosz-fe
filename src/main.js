f5b1acd1bcc20f2c8f00162931c944ca04b94c81
Takács Tamás <tamas.takacs91@gmail.com>
2020-09-14 21:43:36 +0200

    performance

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
-import i18n from './i18n';
 
 const register = (Vue) => (path) => {
   const name = (path) => path.split('/')[path.split('/').length - 1];
@@ -41,6 +40,5 @@ new Vue({
   router,
   store,
   vuetify,
-  i18n,
   render: (h) => h(App),
 }).$mount('#app');
