6456234ded0b73fe39ce6c0f6cc8cf1e8d417f97
Takács Tamás <tamas.takacs91@gmail.com>
2020-09-18 21:33:30 +0200

    optimized

---
+++ src/mixins/icons.js
@@ -0,0 +1,47 @@
+// import {
+//   mdiTeach,
+//   mdiEarth,
+//   mdiPresentationPlay,
+//   mdiMedicalBag,
+//   mdiAccountGroup,
+//   mdiMusicCircle,
+//   mdiMenuDown,
+//   mdiCalendarRangeOutline,
+//   mdiAccountCheckOutline,
+//   mdiLogout,
+//   mdiAccountMultipleCheckOutline,
+//   mdiDotsVertical,
+//   mdiEye,
+//   mdiEyeOff,
+//   mdiExclamation,
+//   mdiEmail,
+//   mdiSearchWeb,
+//   mdiCheck,
+//   mdiWindowClose,
+// } from '@mdi/js';
+
+// export const iconSet = {
+//   data() {
+//     return {
+//       mdiTeach,
+//       mdiEarth,
+//       mdiPresentationPlay,
+//       mdiMedicalBag,
+//       mdiAccountGroup,
+//       mdiMusicCircle,
+//       mdiMenuDown,
+//       mdiCalendarRangeOutline,
+//       mdiAccountCheckOutline,
+//       mdiLogout,
+//       mdiAccountMultipleCheckOutline,
+//       mdiDotsVertical,
+//       mdiEye,
+//       mdiEyeOff,
+//       mdiExclamation,
+//       mdiEmail,
+//       mdiSearchWeb,
+//       mdiCheck,
+//       mdiWindowClose
+//     };
+//   },
+// };
