a4b419dc49eff71f32c9a59494fcafaa796884ae
Takács Tamás <tamas.takacs91@gmail.com>
2020-10-08 16:25:17 +0200

    trial disqus address

--- src/views/JazzPresso.vue
+++ src/views/JazzPresso.vue
@@ -37,7 +37,6 @@ export default {
   mixins: [titleGenerator],
   created() {
     document.title = setPageTitle();
-    this.$forceUpdate();
   },
   methods: {
     getTitle() {
@@ -46,11 +45,8 @@ export default {
   },
   mounted() {
     if (localStorage.getItem('reloaded')) {
-      // The page was just reloaded. Clear the value from local storage
-      // so that it will reload the next time this page is visited.
       localStorage.removeItem('reloaded');
     } else {
-      // Set a flag so that we know not to reload the page twice.
       localStorage.setItem('reloaded', '1');
       location.reload();
     }
