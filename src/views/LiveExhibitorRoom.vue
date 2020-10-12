3efe677fc3d58889dfd136430ae11687bcf9c1a3
Takács Tamás <tamas.takacs91@gmail.com>
2020-10-12 20:03:40 +0200

    live rooms

--- src/views/LiveExhibitorRoom.vue
+++ src/views/LiveExhibitorRoom.vue
@@ -9,8 +9,70 @@
           </div>
         </v-col>
       </v-row>
-      <v-row align="center" justify="center" class="mt-5"> </v-row>
-      <v-row align="center" justify="center"> </v-row>
+      <v-row align="center" justify="center" class="mt-10">
+        <v-col>
+          <v-row align="center" justify="center">
+            <v-img
+              alt="Erodium"
+              class="shrink pointer rounded-lg hidden-sm-and-down"
+              contain
+              src="/logos/1Erodium.png"
+              transition="scale-transition"
+              max-width="400"
+            />
+            <v-img
+              alt="Erodium"
+              class="shrink pointer rounded-lg hidden-md-and-up"
+              contain
+              src="/logos/1Erodium.png"
+              transition="scale-transition"
+              max-width="300"
+            />
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row align="center" justify="center">
+        <v-col md="6">
+          <v-row align="center" justify="center">
+            <v-img
+              alt="Erodium"
+              class="shrink pointer rounded-lg hidden-sm-and-down"
+              contain
+              src="/logos/5Netdoktor.jpg"
+              transition="scale-transition"
+              max-width="400"
+            />
+            <v-img
+              alt="Erodium"
+              class="shrink pointer rounded-lg hidden-md-and-up"
+              contain
+              src="/logos/5Netdoktor.jpg"
+              transition="scale-transition"
+              max-width="300"
+            />
+          </v-row>
+        </v-col>
+        <v-col md="6">
+          <v-row align="center" justify="center">
+            <v-img
+              alt="Erodium"
+              class="shrink pointer rounded-lg hidden-sm-and-down"
+              contain
+              src="/logos/2Roche.png"
+              transition="scale-transition"
+              max-width="350"
+            />
+            <v-img
+              alt="Erodium"
+              class="shrink pointer rounded-lg hidden-md-and-up"
+              contain
+              src="/logos/2Roche.png"
+              transition="scale-transition"
+              max-width="300"
+            />
+          </v-row>
+        </v-col>
+      </v-row>
     </v-container>
     <more-info></more-info>
   </div>
